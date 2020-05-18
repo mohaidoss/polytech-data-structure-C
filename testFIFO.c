ce10d5f2d7a48615edb1719aac5ea04c2ed52c3b
mjedny <mohamed.jedny@polytech-lille.fr>
2020-05-18 08:53:35 +0200

    oui

--- testFIFO.c
+++ testFIFO.c
@@ -1,14 +1,22 @@
 #include "file_int.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int main ()
-{
-  struct file_int F;
-  init_file_int (&F);
-  enfiler_int(&F,7);
-  enfiler_int(&F,77);
-  imprimer_file_int(&F);
-  int v;
-  defiler_int(&v,&F);
-  imprimer_file_int(&F);
-  return 0;
+int main(){
+	printf("a\n");
+	struct file_int F;
+	printf("b\n");
+	init_file_int(&F);
+	printf("c\n");
+	enfiler_int(&F,7);
+	imprimer_file_int(&F);
+	printf("7\n");
+	enfiler_int(&F,77);
+	imprimer_file_int(&F);
+	int v;
+	printf("sa\n");
+	defiler_int(&v,&F);
+	imprimer_file_int(&F);
+	clear_file_int(&F);
+	return 0;
 }
