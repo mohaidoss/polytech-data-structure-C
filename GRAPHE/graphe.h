1f8ba7b36d679135653ff9b4aa317845ba55a846
mjedny <mohamed.jedny@polytech-lille.fr>
2020-05-24 10:02:51 +0200

    ouais

--- GRAPHE/graphe.h
+++ GRAPHE/graphe.h
@@ -1,1 +1,19 @@
-caca
+#include "liste_int.h"
+
+struct sommet
+{
+	int num;
+	struct liste_int succ;
+};
+
+struct graphe
+{
+	int nb_sommets;
+	struct sommet* tab[nb_sommets];
+};
+
+void init_graphe (struct graphe*);
+void ajout_succ (struct graphe*, int prec, int succ);
+void afficher_graphe (struct graphe*);
+void charger_graphe(struct graphe*);
+void clear_graphe(struct graphe*);
