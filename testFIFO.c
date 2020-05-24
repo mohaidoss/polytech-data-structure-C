417507b2eb0cf9d46710626f1662e33fcbd26aef
mjedny <mohamed.jedny@polytech-lille.fr>
2020-05-24 08:59:27 +0200

    le7wa a les amis

--- testFIFO.c
+++ testFIFO.c
@@ -8,8 +8,22 @@ int main(){
 	init_file_int(&F);
 
 	enfiler_int(&F,7);
+	imprimer_file_int(&F);
 	enfiler_int(&F,77);
 
+	imprimer_file_int(&F);
+	enfiler_int(&F,42);
+	enfiler_int(&F,6);
+	imprimer_file_int(&F);
+
+
+	int a;
+	defiler_int(&a,&F);
+	printf("%d\n",a);
+	enfiler_int(&F,10);
+	int b;
+	defiler_int(&b,&F);
+	printf("%d\n",b);
 	imprimer_file_int(&F);
 
 	clear_file_int(&F);
