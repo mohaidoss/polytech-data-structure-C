5e5e2e48281a5843f1d3f66790f97c4bbf7451ef
Mehdi Moussaif <mehdi.moussaif.etu@univ-lille.fr>
2020-05-25 09:58:14 +0200

    Manque charger

--- GRAPHE/graphe1.c
+++ GRAPHE/graphe1.c
@@ -11,31 +11,21 @@ struct Sommet* init_graphe(int size){
 	int i;
 	for(i=0;i<size;i++){
 		g[i].valeur = i;
-		g[i].succ = (struct liste_int*)0;
+		g[i].succ = malloc(sizeof(struct liste_int));
+		
 	}
 	return g;
 }
 
 void ajout_succ(struct Sommet* g, int s1, int s2){
-	if(g[s1].succ == (struct liste_int*)0){
-		struct liste_int* nvarc = malloc(sizeof(struct liste_int));
-		init_liste_int(nvarc);
-		ajout_en_queue_liste_int(nvarc,s2);
-		g[s1].succ = nvarc;
-	}
-	else	ajout_en_queue_liste_int(g[s1].succ,s2);
+	ajout_en_queue_liste_int(g[s1].succ,s2);
 }
 
 void afficher_graphe(struct Sommet* g,int size){
 	int i;
 	for(i=0;i<size;i++){
 		printf("%d : ",g[i].valeur);
-		if (g[i].succ != ((struct liste_int*)0)) {
-		    print_liste_int(g[i].succ);
-		}
-		else {
-		    printf("[ ]\n");
-		}
+		print_liste_int(g[i].succ);
 	}
 }
 
